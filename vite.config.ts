82fdbcdf29aeeb8da31872c8fd7804f22c10914e
Prog. Kanishk Raj <programmerkr.123@gmail.com>
2025-09-07 09:12:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/pitchforge/" : "/",
+  base: mode === "production" ? "/slide-scribe-app/" : "/",
   server: {
     host: "::",
     port: 8080,
